8a0bf08c403d5e56b46db9291e081368f78d0be8
fruiedem <fruiedem@gmail.com>
2025-05-18 22:56:27 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Start React 200!</h1>
+        <p>CSS 적용하기기</p>
+      </div>
+    </>
+  )
+}
+
+export default App
